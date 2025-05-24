7c3c2697d43886d9b657b1cc619c02022debb4c8
Qinruojun <adventurer1337@outlook.com>
2025-05-24 13:02:28 +0800

    init

---
+++ Grid.cpp
@@ -0,0 +1,5 @@
+#include "Grid.h"
+
+Grid::Grid(QWidget *parent)
+    : QWidget{parent}
+{}
---
+++ Grid.h
@@ -0,0 +1,15 @@
+#ifndef GRID_H
+#define GRID_H
+
+#include <QWidget>
+
+class Grid : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Grid(QWidget *parent = nullptr);
+
+signals:
+};
+
+#endif // GRID_H
